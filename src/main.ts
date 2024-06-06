82dcfa7d838d026db4db2d5b25dbb8250094c7a5
Yurkin Daniil <zaobaoo@yandex.kz>
2024-06-06 20:03:25 +0500

    1002. Find Common Characters

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+function commonChars(words: string[]): string[] {
+  const wordsSplit = words.map((item) => item.split(""));
+
+  return wordsSplit.reduce((acc, current) => {
+    const newArray = [];
+    for (let char of current) {
+      if (acc.includes(char)) {
+        newArray.push(char);
+        const index = acc.findIndex((elem) => elem === char);
+        delete acc[index];
+      }
+    }
+    return newArray;
+  }, wordsSplit[0]);
+}
+
+commonChars(["bella", "label", "roller"]);
