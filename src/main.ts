7f3658b32c2002aba4010a2097f2ad6c42439669
Yurkin Daniil <zaobaoo@yandex.kz>
2024-06-06 20:42:23 +0500

    Start

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 // https://leetcode.com/problems/palindrome-number/description/
 
-function isPalindrome(x: number): boolean {}
+function isPalindrome(x: number): boolean {
+  // const array: number[];
 
-isPalindrome();
+  if (x === 0) return true;
+  if (x < 0) return false;
+
+  const count = Math.floor(Math.log10(x) + 1);
+
+  for (let i = count; count > 0; --i) {
+    console.log(i);
+  }
+
+  console.log(count);
+}
+
+isPalindrome(123432);
