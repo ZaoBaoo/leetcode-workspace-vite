09a08de61a64408115a4ddc93d6256a1c8a32669
Yurkin Daniil <zaobaoo@yandex.kz>
2024-06-21 17:15:58 +0500

    Не решил

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 // https://leetcode.com/problems/palindrome-number/description/
 
 function isPalindrome(x: number): boolean {
-  // const array: number[];
-
-  if (x === 0) return true;
   if (x < 0) return false;
+  const arr: number[] = [];
 
-  const count = Math.floor(Math.log10(x) + 1);
+  let length = Math.floor(Math.log10(x) + 1);
 
-  for (let i = count; count > 0; --i) {
-    console.log(i);
+  let cof = 10;
+
+  for (let i = 10; length > 0; i *= cof) {
+    const num = Math.floor(x % i) | 0;
+    arr.push(num);
+    length--;
   }
 
-  console.log(count);
+  console.log(arr);
 }
 
-isPalindrome(123432);
+isPalindrome(123);
