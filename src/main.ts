9e1f32af160fcd2a83a0d00aaa76561d1da55c31
Yurkin Daniil <zaobaoo@yandex.kz>
2024-07-01 16:10:25 +0500

    Add Two Promises

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-// Ссылка на задачу
+// https://leetcode.com/problems/add-two-promises/description/
+
+type P = Promise<number>;
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+  return Promise.all([promise1, promise2]).then((data) =>
+    data.reduce((prev, next) => prev + next, 0),
+  );
+}
