8bc0eb8b4c0dc1cbf2031d56c89eb105660a7fce
Yurkin Daniil <zaobaoo@yandex.kz>
2024-07-01 15:59:38 +0500

    Score of a String

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-// Ссылка на задачу
+// https://leetcode.com/problems/score-of-a-string/
+
+function scoreOfString(s: string): number {
+  const getCharCode = (char: string) => char.charCodeAt(0);
+  const arrNumber: number[] = [];
+
+  for (let i = 0; i < s.length - 1; i++) {
+    const startLine = getCharCode(s[i]);
+    const endLine = getCharCode(s[i + 1]);
+    arrNumber.push(startLine - endLine);
+  }
+
+  return arrNumber.map(Math.abs).reduce((prev, next) => prev + next, 0);
+}
+
+scoreOfString("zaz");
