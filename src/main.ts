0ad46a52a0250d8f937ab36c9bc512177f2c377c
Yurkin Daniil <zaobaoo@yandex.kz>
2024-07-01 14:02:54 +0500

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-// Ссылка на задачу
+// https://leetcode.com/problems/to-lower-case/description/
+
+function toLowerCase(s: string): string {
+  const strSplit = s.split("");
+  const arrChars = strSplit.map((char) => char.charCodeAt(0));
+  const isUpper = (code: number) => code >= 65 && code <= 90;
+  const toLower = (code: number) => String.fromCharCode(code);
+
+  return arrChars.reduce(
+    (prev, next) =>
+      (prev += isUpper(next) ? toLower(next + 32) : toLower(next)),
+    "",
+  );
+}
+
+toLowerCase("Hello");
