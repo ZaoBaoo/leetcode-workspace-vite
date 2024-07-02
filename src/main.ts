97e0617bcc47ba4dc219bbef0ed9fdcc566b2c56
Yurkin Daniil <zaobaoo@yandex.kz>
2024-07-02 10:31:37 +0500

    Array Prototype Last

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-// Ссылка на задачу
+// https://leetcode.com/problems/array-prototype-last/
+
+interface Array<T> {
+  last(): T | -1;
+}
+
+Array.prototype.last = function () {
+  return this.length === 0 ? -1 : this[this.length - 1];
+};
+
+console.log([1, 2, 3].last());
