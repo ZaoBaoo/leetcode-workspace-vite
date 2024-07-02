2d3a09a7bfc5e3231791c8c9c85ec2dd1b7e7d2d
Yurkin Daniil <zaobaoo@yandex.kz>
2024-07-02 12:06:51 +0500

    Debounce

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-// Ссылка на задачу
+// https://leetcode.com/problems/debounce/
+
+type F = (...args: number[]) => void;
+
+function debounce(fn: F, t: number): F {
+  let timerId: ReturnType<typeof setTimeout>;
+
+  return function (...args) {
+    if (timerId) clearTimeout(timerId);
+
+    timerId = setTimeout(() => fn(...args), t);
+  };
+}
+
+const fn1 = debounce(console.log, 1000);
+
+fn1(666);
+fn1(777);
