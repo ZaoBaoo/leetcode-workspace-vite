60c1838f63eaaa72b6129155ef6efb0e7e074a6d
Yurkin Daniil <zaobaoo@yandex.kz>
2024-07-02 12:12:26 +0500

    Sleep

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-// Ссылка на задачу
+// https://leetcode.com/problems/sleep/
+
+async function sleep(millis: number): Promise<void> {
+  return new Promise((resolve, _) => setTimeout(resolve, millis));
+}
+
+sleep(2000).then(() => console.log(123));
+
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
