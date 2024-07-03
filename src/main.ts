02476c0b77dc9728c945cfba1c10784c30666678
Yurkin Daniil <zaobaoo@yandex.kz>
2024-07-03 10:00:33 +0500

    Find Minimum Operations to Make All Elements Divisible by Three

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-// Ссылка на задачу
+// https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/description/
+
+function minimumOperations(nums: number[]): number {
+  return nums.reduce((prev, next) => prev + (next % 3 && 1), 0);
+}
+
+console.log(minimumOperations([1, 2, 3, 4]));
